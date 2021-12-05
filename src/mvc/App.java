32a14b9003a24f95ceab77ae61c22a4b36a478d3
=lazofernando <45129156@certus.edu.pe>
2021-12-04 21:31:54 -0500

    prueba

--- src/mvc/App.java
+++ src/mvc/App.java
@@ -1,12 +1,17 @@
-
 package mvc;
 
-
+import controller.Controller;
+import model.Model;
+import view.Vista;
 
 public class App {
     public static void main(String[] args) {
+        Model mod = new Model();
+        Vista view = new Vista();
         
-   
+        Controller ctrl = new Controller(view,mod);
+        ctrl.iniciar();
+        view.setVisible(true);
     }
 }
     
